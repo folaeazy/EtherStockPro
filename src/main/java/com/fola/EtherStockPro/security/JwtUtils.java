8b18d78e4d9bac63643bdc0c8ecd9463983b7248
folaeazy <folaeazy0423@gmail.com>
2025-06-18 13:18:43 +0100

    added jwtfilter and config

---
+++ src/main/java/com/fola/EtherStockPro/security/JwtUtils.java
@@ -0,0 +1,58 @@
+package com.fola.EtherStockPro.security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+public class JwtUtils {
+
+    private static final long EXPIRATION_TIME_IN_MILLISEC = 100L * 60L * 60L * 24L * 30L * 6L;
+
+    private SecretKey key;
+
+    public JwtUtils( @Value("$(secretJwtPrivateKey)") String secretJwtPrivateKey) {
+       this.key = Keys.hmacShaKeyFor(secretJwtPrivateKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MILLISEC))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return  extractClaims(token,  Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction
+                .apply(Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload());
+    }
+
+    public boolean isTokenValid(String token , UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+
+}
